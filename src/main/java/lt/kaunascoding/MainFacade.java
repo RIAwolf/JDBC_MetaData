2dd8ba1b2b06de848c21f2a03e18814dfc3061e2
Martynas <packmaster@riawolf.com>
2018-08-02 11:03:50 +0300

    first commit

---
+++ src/main/java/lt/kaunascoding/MainFacade.java
@@ -0,0 +1,10 @@
+package lt.kaunascoding;
+
+import lt.kaunascoding.view.DBView;
+
+public class MainFacade {
+    public static void main (String[] args){
+        DBView objektas = new DBView();
+        objektas.dirbam();
+    }
+}
---
+++ src/main/java/lt/kaunascoding/model/DBSingleton.java
@@ -0,0 +1,70 @@
+package lt.kaunascoding.model;
+
+import java.sql.*;
+
+public class DBSingleton {
+
+    public static final String DB_HOST_NAME = "192.168.100.100";
+    public static final String DB_PORT = "3306";
+
+    public static final String DB_NAME = "mysql";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "root";
+
+
+
+    private static  DBSingleton instance = null;
+
+    public static DBSingleton getInstance() {
+        if(instance == null){
+            instance = new DBSingleton();
+        }
+        return instance;
+    }
+    Connection connection=null;
+    private DBSingleton(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection= DriverManager.getConnection("jdbc:mysql://"+DB_HOST_NAME+":"+DB_PORT+"/"+DB_NAME, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Statement statement= connection.createStatement();
+            ResultSet resultSet= statement.executeQuery("SELECT * FROM `user`;");
+            while(resultSet.next()){
+                System.out.println(resultSet.getString(1)+ " "+resultSet.getString(2));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printQueryResult(String query){
+        Statement st = null;
+
+        try {
+            st=connection.createStatement();
+            ResultSet resultSet= st.executeQuery(query);
+            ResultSetMetaData metaData =resultSet.getMetaData();
+            while(resultSet.next()) {
+                String eilute = "";
+                for (int j = 1; j <= metaData.getColumnCount(); j++) {
+                    eilute += resultSet.getString(j)+" ";
+
+                }
+                System.out.println(eilute);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
